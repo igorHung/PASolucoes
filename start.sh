08b2e948a88e4ab2e09e1fd3c73dd1192cc8d99a
erick lucio <ericksi@synnex.com>
2021-01-24 01:04:28 -0300

    .

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 echo initializing dependencies
-bash -c 'cd /root/app/BackEndPaSolucoes && npm install  \
-    cd /root/app/FrontEndPaSolucoes & npm install '
+bash 'cd /root/app/BackEndPaSolucoes && npm install  \
+    cd /root/app/FrontEndPaSolucoes && npm install '
 echo building Front
-bash 'cd /root/app/FrontEndPaSolucoes & npm run build '
+bash 'cd /root/app/FrontEndPaSolucoes && npm run build '
 echo Starting Services
 bash -c 'cd /root/app/BackEndPaSolucoes && npm start  \
-    cd /root/app/FrontEndPaSolucoes & npm start '
+    cd /root/app/FrontEndPaSolucoes && npm start '
 
 echo Services started
