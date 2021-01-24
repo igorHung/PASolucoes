cc8cb20bc6d50d810b6e7d4231bd17c7ae93904a
erick lucio <ericksi@synnex.com>
2021-01-24 00:50:10 -0300

    24-01-2021: Alterado scripts

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@ echo initializing dependencies
 bash -c 'cd /root/app/BackEndPaSolucoes && npm install  \
     cd /root/app/FrontEndPaSolucoes & npm install '
 echo building Front
-bash 'cd /root/app/FrontEndPaSolucoes & npm install '
+bash 'cd /root/app/FrontEndPaSolucoes & npm run build '
 echo Starting Services
 bash -c 'cd /root/app/BackEndPaSolucoes && npm start  \
     cd /root/app/FrontEndPaSolucoes & npm start '
