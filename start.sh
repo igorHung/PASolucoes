ae6343b3f370d7d2e957ee1c62fd73198464b5c2
erick lucio <ericksi@synnex.com>
2021-01-24 01:19:24 -0300

    24-01-2021: Finalizado estrutura do front e backend

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ bash -c 'cd /root/app/BackEndPaSolucoes && npm install  \
 echo building Front
 bash -c 'cd /root/app/FrontEndPaSolucoes && npm run build '
 echo Starting Services
-bash -c 'cd /root/app/BackEndPaSolucoes && npm start  \
+bash -c 'cd /root/app/BackEndPaSolucoes && npm start &\
     cd /root/app/FrontEndPaSolucoes && npm start '
 
 echo Services started
