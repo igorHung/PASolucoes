c9cff3ba926f8e121d494e9799f6241f28b9d53b
erick lucio <ericksi@synnex.com>
2021-01-27 09:48:52 -0300

    .

--- scripts/startAll.sh
+++ scripts/startAll.sh
@@ -1,5 +1,5 @@
 echo Starting Services
 tmux new-session -d -s backEndcall 'cd /root/app/scripts/startBack.sh'
-tmux new-session -d -s frontEndCall'cd /root/app/scripts/startFront.sh'
+tmux new-session -d -s frontEndCall 'cd /root/app/scripts/startFront.sh'
 
 echo Services started
