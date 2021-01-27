14ca99206e50ffeb52ebaa032e50050a7c96ba51
erick lucio <ericksi@synnex.com>
2021-01-27 09:48:28 -0300

    .

--- scripts/buildAll.sh
+++ scripts/buildAll.sh
@@ -7,13 +7,13 @@ git stash clear
 git pull origin master
 sleep 30
 
-echo initializing dependencies
+echo installing dependencies
 tmux new-session -d -s installDependenciesCall 'cd /root/app/scripts/installDependencies.sh'
 sleep 10
 tmux new-session -d -s buildingFrontCall 'cd /root/app/scripts/buildFront.sh'
 sleep 10
 echo Starting Services
 tmux new-session -d -s backEndcall 'cd /root/app/scripts/startBack.sh'
-tmux new-session -d -s frontEndCall'cd /root/app/scripts/startFront.sh'
+tmux new-session -d -s frontEndCall 'cd /root/app/scripts/startFront.sh'
 
 echo Services started
